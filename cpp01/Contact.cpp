35f16f955d13a25353484051874fa5b442e1a00d
Nuno Grazina <101281946+nhorta-g@users.noreply.github.com>
2023-05-17 12:51:22 +0100

    Delete cpp01 directory

--- cpp01/Contact.cpp
+++
@@ -1,9 +0,0 @@
-#include "Contact.hpp"
-
-Contact::Contact(void){
-	std::cout<<"Constructor called, contact."<<std::endl;
-}
-
-Contact::~Contact(void){
-	std::cout<<"Destructor called, contact."<<std::endl;
-}
