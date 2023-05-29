73c9407455b50125142c23502c9586c1a869143d
Nuno Grazina <nunogra@gmail.com>
2023-05-29 17:06:44 +0100

    ex00 complited

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -8,15 +8,13 @@ int	main(int ac, char **av)
 	int	i = 1;
 
 	if (ac == 1)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
 
 	while (av[i] && i < ac)
 	{
 		for (int j = 0; j < (int)strlen(av[i]); j++)
 			std::cout << (char)toupper(av[i][j]);
-		std::cout << std::endl;
 		i++;
 	}
+	std::cout << std::endl;
 }
